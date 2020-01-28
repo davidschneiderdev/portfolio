cce8be6294ad00eba8d7b10d1b554aacec452cde
David Schneider <david.jay.schneider@gmail.com>
2020-01-28 09:15:43 -0500

    adds changes to desktopHeader

--- index.js
+++ index.js
@@ -5,13 +5,30 @@ const dropdown = document.querySelector('.js-nav');
 let dropdownClicked = false;
 
 toggle.addEventListener("click", showDropdown);
+dropdown.addEventListener("click", showDropdown);
 
 function showDropdown() {
-    dropdownClicked = !dropdownClicked;
-    console.log('button clicked');
-    if (dropdownClicked === true) {
-        dropdown.style = "display: flex;";
-    } else {
-        dropdown.style = "display: none;";
+    if (!screenSize.matches) {
+        dropdownClicked = !dropdownClicked;
+        console.log('button clicked');
+        if (dropdownClicked === true) {
+            dropdown.style = "display: flex;";
+        } else {
+            dropdown.style = "display: none;";
+        }
     }
 }
+
+var screenSize = window.matchMedia("(min-width: 400px)")
+
+function disableDropdown(screenSize) {
+    if (screenSize.matches) {
+        console.log(`Screen bigger than 400px`);
+        dropdownClicked = false;
+    } else {
+        console.log(`Screen smaller than 400px`);
+    }
+}
+
+disableDropdown(screenSize);
+screenSize.addListener(disableDropdown);
