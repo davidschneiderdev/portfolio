7d6a07d54770510d26240ab3349726bf13766c77
David Schneider <david.jay.schneider@gmail.com>
2020-02-12 16:55:29 -0500

    adds styling to desktop header

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 const toggle = document.querySelector('.js-toggle');
+const mobileHeader = document.querySelector('.js-mobileHeader');
+const desktopHeader = document.querySelector('.js-desktopHeader');
 const dropdown = document.querySelector('.js-nav');
-const contact = document.querySelector('.js-contact');
+const mobileContact = document.querySelector('.js-mobileContact');
+const desktopContact = document.querySelector('.js-desktopContact');
 const contactDropdown = document.querySelector('.js-contactDropdown');
 
 
@@ -10,25 +13,39 @@ let contactClicked = false;
 
 toggle.addEventListener("click", showMenuDropdown);
 dropdown.addEventListener("click", showMenuDropdown);
-contact.addEventListener("click", showContactDropdown);
+mobileContact.addEventListener("click", showContactDropdown);
+desktopContact.addEventListener("click", showContactDropdown);
+
 
 function showMenuDropdown() {
     dropdownClicked = !dropdownClicked;
     if (dropdownClicked === true) {
         contactDropdown.style = "display: none;";
         dropdown.style = "display: flex;";
+        mobileHeader.style = "background-color: rgba(50, 103, 113, 1);"
+        desktopHeader.style = "background-color: rgba(50, 103, 113, 1);"
     } else {
         dropdown.style = "display: none;";
+        mobileHeader.style = "background-color: none;"
+        desktopHeader.style = "background-color: none;"
+        
+
     }
 }
 
 function showContactDropdown() {
+    console.log('contact clicked');
     contactClicked = !contactClicked;
     if (contactClicked === true) {
         dropdown.style = "display: none;";
         contactDropdown.style = "display: flex;";
+        mobileHeader.style = "background-color: rgba(50, 103, 113, 1);"
+        desktopHeader.style = "background-color: rgba(50, 103, 113, 1);"
+
     } else {
         contactDropdown.style = "display: none;";
+        mobileHeader.style = "background-color: none;"
+        desktopHeader.style = "background-color: none;"
     }
 }
 
